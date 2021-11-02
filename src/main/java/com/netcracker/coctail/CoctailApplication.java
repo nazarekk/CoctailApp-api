3e883a6edf167d246d1c9307bccd94baa6768f9f
nazarekk <01102000nz@gmail.com>
2021-11-02 20:27:48 +0200

    init

---
+++ src/main/java/com/netcracker/coctail/CoctailApplication.java
@@ -0,0 +1,13 @@
+package com.netcracker.coctail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoctailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoctailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netcracker/coctail/CoctailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netcracker.coctail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoctailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
