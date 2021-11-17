386f393cb4726772def8aac404d269afe63e30b0
nazarekk <01102000nz@gmail.com>
2021-11-17 20:39:40 +0200

    Configured CORS

--- src/main/java/com/netcracker/coctail/config/SecurityConfig.java
+++ src/main/java/com/netcracker/coctail/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
   private static final String MODERATOR_ENDPOINT = "/api/moderators/**";
   //private static final String ALL_ENDPOINT = "/**";
   private static final String LOGIN_ENDPOINT = "/api/auth/login";
-  //private static final String REG_ENDPOINT = "/api/users/**";
+  private static final String REG_ENDPOINT = "/api/users/**";
   private static final String front_link = "${front_link}";
 
   @Autowired
@@ -48,7 +48,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
         .and()
         .authorizeRequests()
         .antMatchers(LOGIN_ENDPOINT).permitAll()
-        .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
+        .antMatchers(REG_ENDPOINT).permitAll()
+        .antMatchers(ADMIN_ENDPOINT).permitAll()
         .antMatchers(MODERATOR_ENDPOINT).permitAll()
         .anyRequest().authenticated()
         .and()
