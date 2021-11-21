1e451744a6ab9d7454476f8b5be901f91fbddc65
nazarekk <01102000nz@gmail.com>
2021-11-21 21:56:10 +0200

    pmd fixes

--- src/main/java/com/netcracker/coctail/controllers/UserRestController.java
+++ src/main/java/com/netcracker/coctail/controllers/UserRestController.java
@@ -1,14 +1,8 @@
 package com.netcracker.coctail.controllers;
 
-import com.netcracker.coctail.dto.UserDto;
-import com.netcracker.coctail.model.User;
 import com.netcracker.coctail.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
