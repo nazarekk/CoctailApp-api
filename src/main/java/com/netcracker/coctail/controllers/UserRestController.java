529eccfdde14fbcbb0f1da2166e9769d49422fa9
nazarekk <01102000nz@gmail.com>
2021-11-21 21:18:42 +0200

    deploy fix

--- src/main/java/com/netcracker/coctail/controllers/UserRestController.java
+++ src/main/java/com/netcracker/coctail/controllers/UserRestController.java
@@ -27,16 +27,16 @@ public class UserRestController {
         this.userService = userService;
     }
 
-    @GetMapping(value = "{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
-        User user = userService.getUserById(id);
-
-        if (user == null) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-
-        UserDto result = UserDto.fromUser(user);
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
+//    @GetMapping(value = "{id}")
+//    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
+//        User user = userService.getUserById(id);
+//
+//        if (user == null) {
+//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//        }
+//
+//        UserDto result = UserDto.fromUser(user);
+//
+//        return new ResponseEntity<>(result, HttpStatus.OK);
+//    }
 }
