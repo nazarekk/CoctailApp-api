1ea1a02ccb4eab8362fe97503fb5cda83a5d1f9e
nazarekk <01102000nz@gmail.com>
2021-11-21 22:01:02 +0200

    pmd fixes

--- src/main/java/com/netcracker/coctail/controllers/UserRestController.java
+++ src/main/java/com/netcracker/coctail/controllers/UserRestController.java
@@ -1,8 +1,14 @@
 package com.netcracker.coctail.controllers;
 
+import com.netcracker.coctail.dto.UserDto;
+import com.netcracker.coctail.model.User;
 import com.netcracker.coctail.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,23 +20,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/users/")
 @CrossOrigin(origins = "*")
 public class UserRestController {
-    //private final UserService userService;
+    private final UserService userService;
 
     @Autowired
     public UserRestController(UserService userService) {
         this.userService = userService;
     }
 
-//    @GetMapping(value = "{id}")
-//    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
-//        User user = userService.getUserById(id);
-//
-//        if (user == null) {
-//            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//        }
-//
-//        UserDto result = UserDto.fromUser(user);
-//
-//        return new ResponseEntity<>(result, HttpStatus.OK);
-//    }
+    @GetMapping(value = "{id}")
+    public ResponseEntity<UserDto> getUserById(@PathVariable(name = "id") Long id) {
+        User user = userService.getUserById(id);
+
+        if (user == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        UserDto result = UserDto.fromUser(user);
+
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
 }
