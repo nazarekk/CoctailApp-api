21ecaf1c4c2ae7623228b555bb7d77cececaee2e
nazarekk <01102000nz@gmail.com>
2021-11-24 00:58:57 +0200

    fixed cors privacy

--- src/main/java/com/netcracker/coctail/config/SecurityConfig.java
+++ src/main/java/com/netcracker/coctail/config/SecurityConfig.java
@@ -9,11 +9,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
