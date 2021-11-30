63f5d85897b3fe0c7ba45a9ae5bf96bbfa849696
nazarekk <01102000nz@gmail.com>
2021-11-30 02:20:02 +0200

    merge friendlist

--- src/main/java/com/netcracker/coctail/controllers/UserRestController.java
+++ src/main/java/com/netcracker/coctail/controllers/UserRestController.java
@@ -18,6 +18,7 @@ import javax.validation.Valid;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
